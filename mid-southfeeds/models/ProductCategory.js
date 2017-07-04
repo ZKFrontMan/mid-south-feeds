edba3a449aeb3be2bdbe9611208c72392c925a3b
Stephen Ward <smw0036@tigermail.auburn.edu>
2017-07-04 15:41:58 +0000

    product category bag images

--- mid-southfeeds/models/ProductCategory.js
+++ mid-southfeeds/models/ProductCategory.js
@@ -8,12 +8,21 @@ var Types = keystone.Field.Types;
 
 var ProductCategory = new keystone.List('ProductCategory');
 
+var storage = new keystone.Storage({
+	adapter: keystone.Storage.Adapters.FS,
+	fs: {
+		path: 'public/images/uploads',
+		publicPath: '/public/images/uploads/',
+	}
+});
+
 ProductCategory.add({
 	name: { type: String },
 	isVisible: { type: Types.Boolean },
 	description: { type: Types.Html, wysiwyg: true },
 	categories: { type: Types.Relationship, ref: 'ProductCategory', many: true },
-	label: { type: String }
+	label: { type: String },
+	bagImage: { type: Types.File, storage: storage }
 });
 
 ProductCategory.defaultColumns = 'name, categories';
